c834210b472d23c6af04fb3d9650df78a095eb76
kanana00-00 <victoria.mutwiri@strathmore.edu>
2022-10-26 20:01:22 +0300

    updates today

---
+++ 1_output.py
@@ -0,0 +1,9 @@
+# output in python
+# the print () function is used to display output on the screen.
+print ("hello world")
+
+#python statements have no terminator, as in no semicolon.
+
+print (2+2)
+
+print( 10 % 3)
---
+++ 2_datatypes.py
@@ -0,0 +1,30 @@
+#datatypes on python
+
+
+#primitive datatypes
+
+x = 5 #integer dtype
+y = 2.5 #float dtype
+
+#python allows variables declarations on the same line.
+#x,y=5,2.5
+
+# in the case of a cube or many sides with the same number then python allows you to do so;
+a = b = c = 3
+
+my_string = ("hello there") #str datatype
+is_true = True #boolean datatype
+is_false = False #boolean datatype
+my_string = [1,2,3] #list dtype
+none_type = None 
+my_char = ('a')
+
+print(type (x))
+print(type(y))
+print (type (a))
+print (type(my_string))
+print (type (is_true))
+print (type(my_char))
+
+#be consistent with things like single and double quotes.
+
