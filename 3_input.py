43dde478873a79d584d80290c15556eb7a4e6f78
kanana00-00 <victoria.mutwiri@strathmore.edu>
2022-11-02 18:49:20 +0300

    2/11/22

---
+++ 3_input.py
@@ -0,0 +1,25 @@
+#input in python
+# used to capture input and stored input in the application
+
+print("User profile Application")
+
+# for the below code to work you just need to remove the hash where necessary
+first_name = input("enter your first name")
+# the prompt is make the user aware of what information they are being asked for
+last_name = input("enter your last name")
+Occupation = input("what do you do for a living?")
+
+print("Your first name is, " + first_name)
+print("Your last name is, " + last_name)
+print("Your Occupation is, " + Occupation)
+
+# another way of outputing in python is through formatted  strings
+
+print(f"Your First name is, {first_name} and your Job is, {Occupation}")
+
+# you put f right before the txt in brackets
+
+# handling non string input
+age = int(input("please enter your age:"))
+
+print(f"In two years, your age will be : {age+2}")
