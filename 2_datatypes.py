a854cecc786b8f3f29886b22b9830112135435ed
kanana00-00 <victoria.mutwiri@strathmore.edu>
2022-11-02 18:09:26 +0300

    2/11/22 updates

--- 2_datatypes.py
+++ 2_datatypes.py
@@ -1,30 +1,32 @@
-#datatypes on python
+# datatypes on python
 
 
-#primitive datatypes
+# primitive datatypes
 
-x = 5 #integer dtype
-y = 2.5 #float dtype
+x = 5  # integer dtype
+y = 2.5  # float dtype
 
-#python allows variables declarations on the same line.
-#x,y=5,2.5
+# python allows variables declarations on the same line.
+# x,y=5,2.5
 
 # in the case of a cube or many sides with the same number then python allows you to do so;
 a = b = c = 3
 
-my_string = ("hello there") #str datatype
-is_true = True #boolean datatype
-is_false = False #boolean datatype
-my_string = [1,2,3] #list dtype
-none_type = None 
+my_string = ("hello there")  # str datatype
+is_true = True  # boolean datatype
+is_false = False  # boolean datatype
+my_string = [1, 2, 3]  # list dtype
+none_type = None
 my_char = ('a')
 
-print(type (x))
+# for the variables such as my_string the underscore(_) is important
+# the oart above is sorta like declaring the variables and the lower part is the output part of what we declared.
+
+print(type(x))
 print(type(y))
-print (type (a))
-print (type(my_string))
-print (type (is_true))
-print (type(my_char))
+print(type(a))
+print(type(my_string))
+print(type(is_true))
+print(type(my_char))
 
-#be consistent with things like single and double quotes.
-
+# be consistent with things like single and double quotes.
